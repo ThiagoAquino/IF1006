383d7cb4afcf88b817c72b94228b7b75f1c08f0f
ThiagoAquino <thiagoaquiino@gmail.com>
2017-05-22 19:24:14 -0300

    Função no shell

--- imprimirString.sh
+++ imprimirString.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-VAR = "Mensagem a ser impressa"
-echo ${#VAR}
+function imprimirString (){
+  echo -e "\n\nA string digitada foi $1 \n\n"
+}
+
+echo -n "Qual a string que vai ser impressa?"
+read _msg
+
+imprimirString "$_msg"
